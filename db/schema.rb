723a018ed21032cc01bf01484037d6db52f3ce7e
PawelBelczyk <227667461+PawelBelczyk@users.noreply.github.com.>
2026-05-17 12:03:12 +0200

    Add admin field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_15_091501) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_17_100109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_15_091501) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "password_digest", null: false
